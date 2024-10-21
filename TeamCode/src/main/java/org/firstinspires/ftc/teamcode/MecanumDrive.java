49abebd56303bc325e216326e60bc0d13f1101e7
Beatrice Gilfix <gilfixbea@gmail.com>
2024-10-21 10:59:11 -0700

    THIS BUILD DOES NOT WORK!! :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class MecanumDrive extends LinearOpMode {
@@ -35,46 +37,56 @@ public class MecanumDrive extends LinearOpMode {
         frontRightMotor = hardwareMap.dcMotor.get("FRM");
         backRightMotor = hardwareMap.dcMotor.get("BRM");
 
-        DcMotor shaftMotor = hardwareMap.dcMotor.get("shaftMotor");
+        DcMotor towerMotor = hardwareMap.dcMotor.get("shaftMotor");
         DcMotor slideMotor = hardwareMap.dcMotor.get("slideMotor");
+        //DcMotor climbMotor = hardwareMap.dcMotor.get("climbMotor");
 
-        //I think right side is supposed to be flipped? (yes it is)
+        CRServo intakeServo = hardwareMap.crservo.get("intakeServo");
+        //Motor Configs
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        //Should I set the whole drivetrain to brake?
+        //climbMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        towerMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
-        if (isStopRequested()) return; //The stop button
+        if (isStopRequested()) return;
 
-        //(Default) Variables that need to be initialized OUTSIDE of the loop (so like not redefined a million billion times over)
+        //(Default) Variables that can be initialized OUTSIDE of the loop (so like not redefined a million billion times over)
         boolean low_power_mode = false;
         boolean wheelsUnlocked = true;
-
+        double power_limiter;
+        double y;
+        double x;
+        double rx;
+        double denominator;
+        double frontLeftPower;
+        double backLeftPower;
+        double frontRightPower;
+        double backRightPower;
         uplink("Overview", "All components successfully initialized!");
 
         while (opModeIsActive()) { //Primary loop
 
-            double power_limiter = low_power_mode ? limitingValue : 1;
+            power_limiter = low_power_mode ? limitingValue : 1;
 
             //IF low power mode is active set value of power limiter to 2, if not keep it as 1
             //Values get divided by low power mode, so having it active cuts speeds in half
 
-            double y = -gamepad1.left_stick_y; //y stick inverted
-            double x = gamepad1.left_stick_x * 1.1; //counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
+            y = -gamepad1.left_stick_y; //y stick inverted
+            x = gamepad1.left_stick_x * 1.1; //counteract imperfect strafing
+            rx = -gamepad1.right_stick_x;
 
             //Handle calculations for each motor in separate variables
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); //Prevent division by 0 by making sure absolute values stay above 1? (I think?)
-            double frontLeftPower = ((y + x + rx) / denominator)/ power_limiter;
-            double backLeftPower = ((y - x + rx) / denominator)/ power_limiter;
-            double frontRightPower = ((y - x - rx) / denominator)/ power_limiter;
-            double backRightPower = ((y + x - rx) / denominator)/ power_limiter;
+            denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); //Prevent division by 0 by making sure absolute values stay above 1? (I think?)
+            frontLeftPower = ((y + x + rx) / denominator)/ power_limiter;
+            backLeftPower = ((y - x + rx) / denominator)/ power_limiter;
+            frontRightPower = ((y - x - rx) / denominator)/ power_limiter;
+            backRightPower = ((y + x - rx) / denominator)/ power_limiter;
 
             //Send power variables to motors
-
-            if (wheelsUnlocked) { //Only pushes power values to the motors if the robot is allowing itself to move
-                beaUtils.pushDrivetrainPower(frontLeftPower,backLeftPower,frontRightPower,backRightPower);
-            }
+            beaUtils.pushDrivetrainPower(frontLeftPower,backLeftPower,frontRightPower,backRightPower);
 
             //slideMotor TESTING
             if (beaUtils.triggerBoolean(gamepad1.left_trigger)) { //Start moving servo to position 0
@@ -92,39 +104,38 @@ public class MecanumDrive extends LinearOpMode {
 
             //shaftMotor TESTING
             if (beaUtils.triggerBoolean(gamepad2.left_trigger)) { //Start moving servo to position 0
-                shaftMotor.setPower(1);
+                towerMotor.setPower(1);
                 uplink("SlideMotor","Power FORWARD");
             }
             else if (beaUtils.triggerBoolean(gamepad2.right_trigger)) { //Start moving servo to position 1
-                shaftMotor.setPower(-1);
+                towerMotor.setPower(-1);
                 uplink("SlideMotor", "Power REVERSE");
             }
             else {
-                shaftMotor.setPower(0);
+                towerMotor.setPower(0);
                 uplink("SlideMotor","Power NEUTRAL");
             }
 
             //Button triggers
-            if (gamepad1.dpad_up) {
+            if (gamepad1.dpad_right) {
                 low_power_mode = true;
                 uplink("PowerStatus", "Low Power Mode ONLINE");
             }
 
-            if (gamepad1.dpad_down) {
+            if (gamepad1.dpad_left) {
                 low_power_mode = false;
                 uplink("PowerStatus", "Low Power Mode OFFLINE");
             }
 
-            if (gamepad1.dpad_left) {
-                wheelsUnlocked = false;
-                uplink("Movement","Wheels LOCKED :(");
+            if (gamepad2.left_bumper) {
+                intakeServo.setPower(1);
             }
-
-            if (gamepad1.dpad_right) {
-                wheelsUnlocked = true;
-                uplink("Movement", "Wheels UNLOCKED :)");
+            else if (gamepad2.right_bumper) {
+                intakeServo.setPower(-1);
             }
-
+            else {
+                intakeServo.setPower(0);
+            }
         }
     }
 }
